58cec12155ce3dc5b10337c84bb92dcff94a99c4
Rachit-Dudhaiya <dudhaiyarachit45@gmail.com>
2026-07-05 16:50:51 +0530

    Solve Problem 1 to 7

---
+++ Problem-3.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial
+{
+    internal class Problem_3
+    {
+        public static void Even()
+        {
+            //Find out whether the given number is odd or even.
+
+            int num;
+            Console.Write("Enter a Number:");
+            num = Convert.ToInt32(Console.ReadLine());
+            if (num % 2 == 0)
+            {
+                Console.WriteLine(num + " is an Even Number");
+            }
+            else
+            {
+                Console.WriteLine(num + " is an Odd Number");
+            }
+        }
+
+    }
+}
---
+++ Problem-6.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial
+{
+    internal class Problem_6
+    {
+        public static void fix()
+        {
+
+            int a, b, c, result;
+            // Missing statement
+            Console.Write("Enter Number 1 : ");
+            String str1 = Console.ReadLine();
+            a = Convert.ToInt32(str1);
+
+            // Missing statement
+            Console.Write("Enter Number 2 : ");
+            String str2 = Console.ReadLine();
+            b = Convert.ToInt32(str2);
+
+            // Missing statement
+            Console.Write("Enter Number 3 : ");
+            String str3 = Console.ReadLine();
+            c = Convert.ToInt32(str3);
+
+
+            result = Sum(a, b, c);
+        }
+            // Missing statement
+           public static int Sum(int x, int y, int z)
+           {
+                int res;
+                res = x + y + z;
+                 Console.WriteLine("Sum : "+res);
+                return res;
+           }
+
+        }
+       
+    }
+
